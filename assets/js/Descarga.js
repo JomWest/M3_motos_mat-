bcf9e4271119ea7d1b460c5c32a467de039121d1
JomWest <145591543+JomWest@users.noreply.github.com>
2024-04-08 22:16:37 -0600

    update

--- assets/js/Descarga.js
+++ assets/js/Descarga.js
@@ -1,5 +1,5 @@
 document.getElementById("descargarPdf").addEventListener("click", function() {
-    var url = "https://drive.google.com/uc?export=download&id=1wKsJ_g238VR9tYvJzqt9rO753KDb9UoU";
+    var url = "https://drive.google.com/file/d/1wKsJ_g238VR9tYvJzqt9rO753KDb9UoU/view?usp=sharing";
     var link = document.createElement("a");
     link.href = url;
     link.download = "requisitos_fidem.pdf";
