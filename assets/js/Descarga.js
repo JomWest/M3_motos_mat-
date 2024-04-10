fe2b8b43ec5af12b5f4cc740b98ee07bc245c2af
JomWest <145591543+JomWest@users.noreply.github.com>
2024-04-10 01:51:38 -0600

    update23558225

--- assets/js/Descarga.js
+++ assets/js/Descarga.js
@@ -27,7 +27,7 @@ var whatsappButtons = document.querySelectorAll(".Ws-button");
 whatsappButtons.forEach(function(button) {
     button.addEventListener("click", function() {
         var modelo = document.querySelector(".Modelo").textContent;
-        var phoneNumber = "50585482224";
+        var phoneNumber = "50588282726";
         var message = "Hola, estoy interesado en el modelo " + modelo + ". ";
         message += "¿Podrías proporcionarme más información sobre el precio?\n";
 
@@ -41,7 +41,7 @@ var whatsappButtons = document.querySelectorAll(".Ws-buttonEspeciasl");
 
 whatsappButtons.forEach(function(button) {
     button.addEventListener("click", function() {
-        var phoneNumber = "50585482224";
+        var phoneNumber = "50588282726";
         var message = "Hola, estoy interesado en conocer mas sobre su tienda. ";
         message += "¿Podrías proporcionarme más información?\n";
 
@@ -57,7 +57,7 @@ var whatsappButtons = document.querySelectorAll(".Ws-button");
 whatsappButtons.forEach(function(button) {
     button.addEventListener("click", function() {
         var modelo = document.querySelector(".Modelo1").textContent;
-        var phoneNumber = "50585482224";
+        var phoneNumber = "50588282726";
         var message = "Hola, estoy interesado en el modelo " + modelo + ". ";
         message += "¿Podrías proporcionarme más información sobre el precio?\n";
 
@@ -71,7 +71,7 @@ var whatsappButtons = document.querySelectorAll(".Ws-button1, .Ws-button10, Ws-b
 whatsappButtons.forEach(function(button) {
     button.addEventListener("click", function() {
         var modelo = document.querySelector(".Modelo").textContent;
-        var phoneNumber = "50585482224";
+        var phoneNumber = "50588282726";
         var message = "Hola, estoy interesado en el modelo " + modelo + ". ";
         message += "¿Podrías proporcionarme más información sobre el precio?\n";
 
